07324ed22913db44e6537fabf3c8d3f80fb0f1d6
cedli74 <166433488+cedli74@users.noreply.github.com>
2025-07-09 11:44:07 +0200

    Update app.ts

--- back/src/app.ts
+++ back/src/app.ts
@@ -17,7 +17,7 @@ dotenv.config();
 
 const app = express();
 app.set('trust proxy', 1);
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 // Configuration CORS
 app.use(cors({
